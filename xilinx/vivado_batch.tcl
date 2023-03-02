489b48bfaa3d7c7bacbc10dcd11b309f1300fe91
Jamieson Olsen <jamieson@fnal.gov>
2023-03-02 10:48:38 -0600

    removed the chipscope ila stuff from the SPI slave logic

--- xilinx/vivado_batch.tcl
+++ xilinx/vivado_batch.tcl
@@ -106,7 +106,7 @@ read_vhdl ../daphne2.vhd
 
 read_ip ../ip/gig_ethernet_pcs_pma_0.xcix
 read_ip ../ip/daphne2_daq_txonly.xcix
-read_ip ../ip/ila_0.xcix
+# read_ip ../ip/ila_0.xcix
 
 # Load IP module as *.xci (loading from XCIX file is preferred over this method)
 #read_ip ../src/ip/gig_ethernet_pcs_pma_0.xci
