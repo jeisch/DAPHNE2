ef10d55c7a2dc580d91d73d4d67bb6539d564a66
Jonathan Eisch <jeisch@pulsar2.fnal.gov>
2023-03-03 22:27:37 -0600

    Add headerless .bit file generation for serial configuration from the microcontroller.

--- xilinx/vivado_batch.tcl
+++ xilinx/vivado_batch.tcl
@@ -165,7 +165,7 @@ report_io -file $outputDir/io.rpt
 
 # generate bitstream...
 
-write_bitstream -force $outputDir/daphne2_$git_sha.bit
+write_bitstream -force -bit_file $outputDir/daphne2_$git_sha.bit
 
 # write out ILA debug probes file
 write_debug_probes -force $outputDir/probes.ltx
